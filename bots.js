535a4b90a6fe33f86d38c43cf95682a42ca71f64
Dilano Doelwijt <d.e.j.doelwijt@student.vu.nl>
2024-04-29 17:42:37 +0200

    trying multiple bots

--- bots.js
+++ bots.js
@@ -1,22 +1,24 @@
 const mineflayer = require('mineflayer')
 
-function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
+class Bot{
+    constructor(Name){
+        this.username=Name 
+        this.joinServer()
+    }
+    joinServer(){
+        this.bot = mineflayer.createBot({"username":this.username})
+    }
+    lifecycle(){
+        this.bot.on('end',()=> {setTimeout(() => this.joinServer, 5000)})
+    }
+
+}
+
+let bots = [];
+
+for(var i = 0; i < 6; i++){
+    bots.push(new Bot('bot number #{i}'))
 }
 
 
-async function createBots(num){
-    for(let i = 0; i < num; i++){
-        const bot = mineflayer.createBot({
-            username: 'Player'
-          })
-        
-        bot.setControlState('forward', true)
-          
-        bot.on('kicked', console.log)
-    
-        await sleep(10000)
-    }    
-}
 
-createBots(5)
