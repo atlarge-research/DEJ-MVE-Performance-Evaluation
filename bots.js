1e969ad62bc9b8141f98bd6b956228d168b72752
Dilano Doelwijt <d.e.j.doelwijt@student.vu.nl>
2024-04-29 16:10:43 +0200

    fix

--- bots.js
+++ bots.js
@@ -4,16 +4,19 @@ function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-for(let i = 0; i < 10; i++){
-    const bot = mineflayer.createBot({
-        username: 'Player'
-      })
-    
-    
-    bot.setControlState('forward', true)
-      
-    bot.on('kicked', console.log)
 
-    await sleep(10000)
+async function createBots(num){
+    for(let i = 0; i < num; i++){
+        const bot = mineflayer.createBot({
+            username: 'Player'
+          })
+        
+        bot.setControlState('forward', true)
+          
+        bot.on('kicked', console.log)
+    
+        await sleep(10000)
+    }    
 }
 
+createBots(5)
