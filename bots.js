0f416bbbd5cebd5afab01d7b89947be4eafe6ba6
Dilano Doelwijt <d.e.j.doelwijt@student.vu.nl>
2024-04-29 21:49:45 +0200

    Added random directions

--- bots.js
+++ bots.js
@@ -1,14 +1,15 @@
 const mineflayer = require('mineflayer')
-
+const directions = ["forward","back","left", "right"]
 
 class bot{
     constructor(botName){
         this.username = botName
         this.joinServer()
+        this.direction = directions[(Math.random() * directions.length) | 0]
     }
     startBotActions(){
         this.bot.once("login",() =>{
-            this.bot.setControlState('forward',true)
+            this.bot.setControlState(this.direction,true)
             this.bot.setControlState('sprint',true)
             this.bot.setControlState('jump',true)
         })
