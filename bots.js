7d308faffdee9a67ea46e1874c9fe5d4c8f9f044
Dilano Doelwijt <d.e.j.doelwijt@student.vu.nl>
2024-04-29 16:05:42 +0200

    Changed bot, and tweaked some other settings

--- bots.js
+++ bots.js
@@ -1,7 +1,19 @@
 const mineflayer = require('mineflayer')
 
-const bot = mineflayer.createBot({
-    username: 'Player'
-  })
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
 
-bot.on('kicked', console.log)
+for(let i = 0; i < 10; i++){
+    const bot = mineflayer.createBot({
+        username: 'Player'
+      })
+    
+    
+    bot.setControlState('forward', true)
+      
+    bot.on('kicked', console.log)
+
+    await sleep(10000)
+}
+
