d950a0c8d1ae1268e6918d7f309d372bccc7690f
Dilano Doelwijt <d.e.j.doelwijt@student.vu.nl>
2024-04-29 15:38:16 +0200

    added more bots

--- bots.js
+++ bots.js
@@ -1,3 +1,3 @@
 const mineflayer = require('mineflayer')
 
-const bot = mineflayer.createBot()
+const bots = [mineflayer.createBot(),mineflayer.createBot(),mineflayer.createBot(),mineflayer.createBot(),mineflayer.createBot()]
