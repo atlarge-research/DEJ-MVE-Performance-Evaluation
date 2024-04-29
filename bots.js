597141c7c538d9ae6ecf70ecd1c289337acdfc3b
Dilano Doelwijt <d.e.j.doelwijt@student.vu.nl>
2024-04-29 21:19:39 +0200

    Working bot test

--- bots.js
+++ bots.js
@@ -4,13 +4,20 @@ const mineflayer = require('mineflayer')
 class bot{
     constructor(botName){
         this.username = botName
-        this.joinServer();
+        this.joinServer()
     }
 
-    initBot(){
+    joinServer(){
         this.bot = mineflayer.createBot({
             "username":this.username
         });
+        bot.startBotActions()
+    }
+
+    startBotActions(){
+        bot.once("login",() =>{
+            bot.setControlState('forward',true)
+        })
     }
 
 
@@ -19,7 +26,7 @@ class bot{
 
 let bots = []
 
-for(let i = 0; i < 10; i++){
+for(let i = 0; i < 50; i++){
     bots.push(new bot("TestBot" + i))
 }
 
