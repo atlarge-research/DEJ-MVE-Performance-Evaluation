38ecb83c17c6d242e1f6914a1216eb5273f53bfd
Dilano Doelwijt <d.e.j.doelwijt@student.vu.nl>
2024-05-15 22:08:13 +0200

    Changed the bots.js

--- bots.js
+++ bots.js
@@ -4,8 +4,7 @@ const directions = ["forward","back","left", "right"]
 class bot{
     constructor(botName){
         this.username = botName
-        this.host = process.argv[2] //needs to be replaced with the server ib_ip
-        console.log(process.argv[2])
+        this.host = process.argv[2]
         this.direction = directions[(Math.random() * directions.length) | 0]
         this.joinServer()
     }
@@ -13,7 +12,6 @@ class bot{
         this.bot.once("login",() =>{
             this.bot.setControlState(this.direction,true)
             this.bot.setControlState('sprint',true)
-            // this.bot.setControlState('jump',true) removed this since its ruining the sprint
         })
     }
     joinServer(){
@@ -25,13 +23,7 @@ class bot{
     }
 }
 const bots = []
-const random = Math.floor((Math.random() * 1000))
-const randomFactor = Math.floor((Math.random() * 200))
-
-const randomNum = random * randomFactor
-
-
 for(let i = 1; i <= 100; i++){
     num =   i + randomNum
-    bots.push(new bot("Bot" + num + "id:" + i))
+    bots.push(new bot("Bot" + i))
 }
