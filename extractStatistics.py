6d82c045e83555ab780928bac0f1c03d161bb2d0
Dilano Doelwijt <d.e.j.doelwijt@student.vu.nl>
2024-06-07 13:56:05 +0200

    Added python script to query the prometheus http api

---
+++ extractStatistics.py
@@ -0,0 +1,10 @@
+import requests
+import sys
+
+
+start_time = sys.argv[1]
+end_time = sys.argv[2]
+
+tick_time_query = "http://localhost:9090/api/v1/query_range?query=net_minecraft_server_Server_averageTickTime&start={1}.999Z&end={1}.000Z&step=5s".format(start_time,end_time)
+
+statistics = requests.get(tick_time_query)
