9695ae0f3b77e683c729ac2b499eedc552f3dca1
Dilano Doelwijt <d.e.j.doelwijt@student.vu.nl>
2024-06-10 01:03:54 +0200

    Further development of the metrics and statistics python scripts to follow what is written in metric_names.json

--- extractStatistics.py
+++ extractStatistics.py
@@ -5,25 +5,19 @@ import json
 start_time = sys.argv[1]
 end_time = sys.argv[2]
 
+with open("metric_names.json") as metric_names_file:
+   metric_types = json.load(metric_names_file)
+   query_responses = [do_query(metric_name=metric['metric_name']).text for metric in metric_types]
+   with open("metrics.json",'w') as file:
+      json.dump(query_responses,file)
 
-metric_names = ["net_minecraft_server_Server_averageTickTime",
-"jvm_gc_collection_seconds_count",
-"jvm_gc_collection_seconds_sum",
-"java_lang_Memory_HeapMemoryUsage_used",
-"jvm_threads_current",
-"rate(jvm_gc_collection_seconds_count[1m])",
-"rate(jvm_gc_collection_seconds_sum[1m])/rate(jvm_gc_collection_seconds_count[1m])",
-"rate(jvm_gc_collection_seconds_sum[1m])",
-]
+
 
 def do_query(metric_name):
-
    query = "http://localhost:9090/api/v1/query_range?query={0}&start={1}.999Z&end={2}.000Z&step=5s".format(metric_name,start_time,end_time)
    return requests.get(query) 
 
 
-query_responses = [do_query(metric_name=name).text for name in metric_names]
 
 
-with open("metrics.json",'w') as file:
-    json.dump(query_responses,file)
+
--- processMetrics.py
+++ processMetrics.py
@@ -3,16 +3,20 @@ import json
 import matplotlib.pyplot as plt
 
 metrics_path = sys.argv[1]
-
-with open(metrics_path) as file:
-    data = json.load(file)
-    for i in range(len(data)):
-        metric_data = data[i]
-        metric_data = json.loads(metric_data)
-        metric_data = metric_data['data']['result'][0]['values']
-        tick_times = [float(x[1]) for x in metric_data]
-        statistics_interval = 5
-        time_points = [x * statistics_interval for x in range(0,len(tick_times))]
-        plt.plot(time_points,tick_times)
-        plt.xlabel("Time in Seconds")
-        plt.savefig("results/plot{0}.png".format(i), bbox_inches='tight')
+with open("metric_names.json") as metric_names_file:
+    metric_types = json.load(metric_names_file)
+    with open(metrics_path) as file:
+        data = json.load(file)
+        for i in range(len(data)):  
+            current_metric_type = metric_types[i]
+            metric_data = data[i]
+            metric_data = json.loads(metric_data)
+            metric_data = metric_data['data']['result'][0]['values']
+            tick_times = [float(x[1]) for x in metric_data]
+            statistics_interval = 5
+            time_points = [x * statistics_interval for x in range(0,len(tick_times))]
+            plt.plot(time_points,tick_times)
+            plt.xlabel("Time in Seconds")
+            plt.ylabel("{0}".format(current_metric_type['y_label']))
+            plt.title("{0}".format(current_metric_type['graph_title']))
+            plt.savefig("results/plot{0}.png".format(current_metric_type['metric_name']), bbox_inches='tight')
