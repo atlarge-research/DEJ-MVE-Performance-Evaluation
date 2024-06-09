a285abdb3e6ac78b13b617add69408fa58b5796e
Dilano Doelwijt <d.e.j.doelwijt@student.vu.nl>
2024-06-10 00:30:12 +0200

    Removed comment

--- processMetrics.py
+++ processMetrics.py
@@ -12,7 +12,7 @@ with open(metrics_path) as file:
         metric_data = metric_data['data']['result'][0]['values']
         tick_times = [float(x[1]) for x in metric_data]
         statistics_interval = 5
-        time_points = [x * statistics_interval for x in range(0,len(tick_times))] #should be times the interval in extract statistics
+        time_points = [x * statistics_interval for x in range(0,len(tick_times))]
         plt.plot(time_points,tick_times)
         plt.xlabel("Time in Seconds")
         plt.savefig("results/plot{0}.png".format(i), bbox_inches='tight')
