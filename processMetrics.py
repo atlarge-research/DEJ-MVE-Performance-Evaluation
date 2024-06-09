21d6fc91870fec0315ce22db88d9a20455b5ba00
Dilano Doelwijt <d.e.j.doelwijt@student.vu.nl>
2024-06-09 13:24:59 +0200

    bug fix

--- processMetrics.py
+++ processMetrics.py
@@ -11,13 +11,13 @@ data = data['data']['result'][0]['values']
 
 tick_times = [float(x[1]) for x in data]
 
-statistics_interval = 1
+statistics_interval = 5
 
 time_points = [x * statistics_interval for x in range(0,len(tick_times))] #should be times the interval in extract statistics
 plt.plot(time_points,tick_times)
 
 
-plt.xlabel("Time in S")
-plt.ylabel("Average Tick time in MS")
+plt.xlabel("Time in Seconds")
+plt.ylabel("Average Tick time in Milliseconds")
 
 plt.savefig('results/tick_time_plt.pdf', bbox_inches='tight')
