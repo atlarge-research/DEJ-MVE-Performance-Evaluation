e85e754b5d67b119f5e1878d7b97bbbb786384bc
Dilano Doelwijt <d.e.j.doelwijt@student.vu.nl>
2024-06-09 17:36:54 +0200

    improved the extract statistics python file to easily allow for more metrics to be extracted

--- extractStatistics.py
+++ extractStatistics.py
@@ -1,13 +1,20 @@
 import requests
 import sys
-
+import json
 
 start_time = sys.argv[1]
 end_time = sys.argv[2]
 
-tick_time_query = "http://localhost:9090/api/v1/query_range?query=net_minecraft_server_Server_averageTickTime&start={0}.999Z&end={1}.000Z&step=5s".format(start_time,end_time)
-tick_time_response = requests.get(tick_time_query)
+metric_names = ["net_minecraft_server_Server_averageTickTime"]
+
+def do_query(metric_name):
+
+   query = "http://localhost:9090/api/v1/query_range?query={0}&start={1}.999Z&end={2}.000Z&step=5s".format(metric_name,start_time,end_time)
+   return requests.get(tick_time_query) 
+
+
+query_responses = [do_query(metric_name=name) for name in metric_names]
 
 
 with open("metrics.json",'w') as file:
-    file.write(tick_time_response.text)
+    json.dump(query_responses,file)
