4d528a141d32e8031ec81682c85302b2bd532410
Dilano Doelwijt <d.e.j.doelwijt@student.vu.nl>
2024-06-10 00:20:29 +0200

    Bug fix for metric processing

--- processMetrics.py
+++ processMetrics.py
@@ -7,12 +7,12 @@ metrics_path = sys.argv[1]
 with open(metrics_path) as file:
     data = json.load(file)
     for i in range(len(data)):
-        data = data[i]
-        data = json.loads(data)
-        data = data['data']['result'][0]['values']
-        tick_times = [float(x[1]) for x in data]
+        metric_data = data[i]
+        metric_data = json.loads(metric_data)
+        metric_data = metric_data['data']['result'][0]['values']
+        tick_times = [float(x[1]) for x in metric_data]
         statistics_interval = 5
         time_points = [x * statistics_interval for x in range(0,len(tick_times))] #should be times the interval in extract statistics
         plt.plot(time_points,tick_times)
         plt.xlabel("Time in Seconds")
-        plt.savefig("results/plot{0}.pdf".format(i), bbox_inches='tight')
+        plt.savefig("results/plot{0}.png".format(i), bbox_inches='tight')
