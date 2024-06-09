980a557b7c13cccba11d9797228f9818ea05333d
Dilano Doelwijt <d.e.j.doelwijt@student.vu.nl>
2024-06-10 01:12:05 +0200

    Fixed small bug

--- extractStatistics.py
+++ extractStatistics.py
@@ -5,6 +5,13 @@ import json
 start_time = sys.argv[1]
 end_time = sys.argv[2]
 
+
+def do_query(metric_name):
+   query = "http://localhost:9090/api/v1/query_range?query={0}&start={1}.999Z&end={2}.000Z&step=5s".format(metric_name,start_time,end_time)
+   return requests.get(query) 
+
+
+
 with open("metric_names.json") as metric_names_file:
    metric_types = json.load(metric_names_file)
    query_responses = [do_query(metric_name=metric['metric_name']).text for metric in metric_types]
@@ -12,10 +19,6 @@ with open("metric_names.json") as metric_names_file:
       json.dump(query_responses,file)
 
 
-
-def do_query(metric_name):
-   query = "http://localhost:9090/api/v1/query_range?query={0}&start={1}.999Z&end={2}.000Z&step=5s".format(metric_name,start_time,end_time)
-   return requests.get(query) 
 
 
 
