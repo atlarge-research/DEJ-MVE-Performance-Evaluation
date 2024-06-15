3d13969ff7de6b12508896221c7b9d6dc09dd735
Dilano Doelwijt <d.e.j.doelwijt@student.vu.nl>
2024-06-16 01:57:33 +0200

    try

--- confex.py
+++ confex.py
@@ -16,26 +16,19 @@ def main():
 
     if experiment["config"]["type"] == "one_by_one":
         res = []
-        parameters = experiment["config"]["params"]
 
         configs_expanded = []
-        for i, param in enumerate(parameters):
-            for v in param["values"]:
-                config = {}
-                config[param["name"]] = v
-                for j, other_param in enumerate(parameters):
-                    if i == j:
-                        continue
-                    config[other_param["name"]] = other_param["default"]
-                if not config in configs_expanded:
-                    configs_expanded.append(config)
-                    
+
+        for i in range(experiment["iterations"] - 1):
+            configs_expanded.append()
+
+
         for i in range(len(configs_expanded)):
             configs_expanded[i]["_index"] = i
-            
+
         l = len(configs_expanded)
-        for i in range(experiment["iterations"] - 1):
-            configs_expanded += copy.deepcopy(configs_expanded[:l])
+
+
         for i, c in enumerate(configs_expanded):
             iter = i // l
             c["_iteration"] = iter
@@ -47,6 +40,7 @@ def main():
                 res.append(exp_config_file.name)
                 for k in x:
                     exp_config_file.write(f"{k}: {x[k]}\n")
+
 
         for p in res:
             print(p)
