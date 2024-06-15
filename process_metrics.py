b51b93f66513c290b36886e27e47a03ce1f1deb8
Dilano Doelwijt <d.e.j.doelwijt@student.vu.nl>
2024-06-15 18:48:04 +0200

    Changed plots and what plots are made

--- process_metrics.py
+++ process_metrics.py
@@ -1,6 +1,7 @@
 import sys
 import json
 import matplotlib.pyplot as plt
+import matplotlib.ticker as plt_tick
 
 statistics_interval = 1
 metrics_path = sys.argv[1]
@@ -16,8 +17,12 @@ with open("metric_names.json") as metric_names_file:
             metric_data = metric_data['data']['result'][0]['values']
             tick_times = [float(x[1]) for x in metric_data]            
             time_points = [x * statistics_interval for x in range(0,len(tick_times))]
-            plt.plot(time_points,tick_times)
+            fig, ax = plt.subplots()
+            ax.plot(time_points,tick_times)
+            # ax.ticklabel_format(useOffset=False, style='plain')
+            ax.get_yaxis().set_major_formatter(plt_tick.FuncFormatter(lambda x, p: format(int(x), ',')))
+
             plt.xlabel("Time in Seconds")
             plt.ylabel("{0}".format(current_metric_type['y_label']))
             plt.title("{0}".format(current_metric_type['graph_title']))
-            plt.savefig("results/plot{0}.png".format(current_metric_type['graph_title'].replace(" ", "").replace("'","")), bbox_inches='tight')
+            fig.savefig("results/plot{0}.png".format(current_metric_type['graph_title'].replace(" ", "").replace("'","")), bbox_inches='tight')
